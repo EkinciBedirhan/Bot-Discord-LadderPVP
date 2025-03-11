6eacca85e8ad43473efa023a8cbdfa9fb13ac24a
EkinciBedirhan <bedirhan.ekinci@hotmail.com>
2025-03-11 17:57:16 +0100

    mise à jour du bot

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const { Client, GatewayIntentBits, EmbedBuilder, SlashCommandBuilder, REST, Routes, PermissionFlagsBits } = require('discord.js');
 const fs = require('fs');
 
